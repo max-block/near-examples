f8e8846d08ec20dcab368770b2010f8a8cb69783
Max Block <maxblock@pm.me>
2021-09-11 14:22:19 +0300

    update

--- 05__nft/src/lib.rs
+++ 05__nft/src/lib.rs
@@ -11,7 +11,6 @@ use near_sdk::{
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LazyOption;
 use near_sdk::json_types::ValidAccountId;
-use near_sdk::serde::Serialize;
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
 
@@ -22,9 +21,7 @@ near_sdk::setup_alloc!();
 pub struct Contract {
     tokens: NonFungibleToken,
     metadata: LazyOption<NFTContractMetadata>,
-    last_minted_id: u32,
     money_wallet: ValidAccountId,
-    limit: u32, // how many tokens can be minted
     price: u128,
 }
 
@@ -45,8 +42,7 @@ impl Contract {
         money_wallet: ValidAccountId,
         name: String,
         symbol: String,
-        limit: u32,
-        price: u128,
+        price: U128,
     ) -> Self {
         let metadata = NFTContractMetadata {
             spec: NFT_METADATA_SPEC.to_string(),
@@ -68,76 +64,30 @@ impl Contract {
                 Some(StorageKey::Approval),
             ),
             metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
-            last_minted_id: 0,
             money_wallet,
-            limit,
-            price,
+            price: price.0,
         }
     }
 
     #[payable]
-    pub fn nft_mint(
-        &mut self,
-        token_id: TokenId,
-        token_owner_id: ValidAccountId,
-        token_metadata: TokenMetadata,
-    ) -> Token {
-        self.tokens
-            .mint(token_id, token_owner_id, Some(token_metadata))
+    pub fn mint_token(&mut self, token_id: TokenId, token_metadata: TokenMetadata) -> Token {
+        self.tokens.mint(
+            token_id,
+            ValidAccountId::try_from(env::current_account_id()).unwrap(),
+            Some(token_metadata),
+        )
     }
 
     #[payable]
-    pub fn buy(&mut self, amount: u32) -> Vec<Token> {
-        if amount + self.last_minted_id > self.limit {
-            env::panic(b"not enough free tokens")
+    pub fn buy_token(&mut self, token_id: TokenId) {
+        if env::attached_deposit() < self.price {
+            env::panic(b"deposit < price");
         }
-
-        if env::attached_deposit() < amount as u128 * self.price {
-            env::panic(b"not enough attached deposit")
+        Promise::new(AccountId::from(self.money_wallet.clone())).transfer(self.price);
+        if env::attached_deposit() > self.price {
+            Promise::new(env::signer_account_id()).transfer(env::attached_deposit() - self.price);
         }
-
-        if env::attached_deposit() > amount as u128 * self.price {
-            // todo: transfer change
-        }
-
-        // Promise::new(env::signer_account_id()).transfer()
-        env::log(b"z1");
-        Promise::new(self.money_wallet.clone().into()).transfer(amount as u128 * self.price);
-        env::log(b"z2");
-
-        let mut result: Vec<Token> = Vec::new();
-        for _ in 0..amount {
-            let token_id = format!("{}", self.last_minted_id + 1);
-            let title = format!("Token {}", token_id);
-            let description = format!("bla bla {}", token_id);
-            let issued_at = "todo".to_string();
-            let metadata = TokenMetadata {
-                title: Some(title),
-                description: Some(description),
-                media: None,
-                media_hash: None,
-                copies: Some(1),
-                issued_at: Some(issued_at),
-                expires_at: None,
-                starts_at: None,
-                updated_at: None,
-                extra: None,
-                reference: None,
-                reference_hash: None,
-            };
-            result.push(self.tokens.mint(
-                token_id,
-                ValidAccountId::try_from(env::signer_account_id()).unwrap(),
-                Some(metadata),
-            ))
-        }
-
-        result
-    }
-
-    fn random_by_timestamp_and_block(&self, max: u64) -> u64 {
-        let base: u128 = (env::block_timestamp() + env::block_index()) as u128;
-        (base % max as u128) as u64
+        self.nft_transfer(ValidAccountId::try_from(env::signer_account_id()).unwrap(), token_id, None, None);
     }
 }
 
